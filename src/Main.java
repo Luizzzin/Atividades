bbe8d36d68b674722166ccfc023ca8157b961b4b
Luizzzin <luizhenribarbodias@gmail.com>
2025-03-15 10:49:53 -0300

    declaração de long

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,11 @@ public class Main {
         int media01 = (numUM+numDois+numTres+numQuatro)/4;
         long media02 = (numUM+numDois+numTres+numQuatro)/4L;
         float media03 = (numUM+numDois+numTres+numQuatro)/4.0f;
-
+        long distancia = 149600000L;
         System.out.println("A media _double_ deste aluno é: "+ media);
         System.out.println("A media _inteira_ deste aluno é: "+ media01);
         System.out.println("A media _long_ deste aluno é: "+ media02);
         System.out.println("A media _float_ deste aluno é: "+ media03);
-
-
+        System.out.println("A distância da Terra ao Sol é: " + distancia + " km.");
     }
 }
